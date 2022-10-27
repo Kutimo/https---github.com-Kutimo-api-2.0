dae638ea830227e48d5919ef7b95a2237a64c658
Kutimo <marius-oliver@hotmail.com>
2022-10-27 09:25:26 +0200

    change in routes.js

--- api/routes.js
+++ api/routes.js
@@ -2,6 +2,6 @@ const express = require("express")
 const router = express.Router();
 const fs = require('fs');
 
-const bookRoute = require("./bookRoute.js") // import book route
-router.use(bookRoute) // use account route
+const bookRoute = require("./bookRoute") // import book route
+router.use(bookRoute) // use book route
 module.exports = router;
